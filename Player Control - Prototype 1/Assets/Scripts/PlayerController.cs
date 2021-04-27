c5d306cba332b83e6b8660ffee5609462944d14e
toydotgame <toydotgame@gmail.com>
2021-04-27 19:33:47 +1000

    Finished Prototype 1

---
+++ Player Control - Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private float speed = 15; // 20 is too fast.
+	private float turnSpeed = 100;
+	private float horizontalInput;
+	private float forwardInput;
+
+	// Start is called before the first frame update
+	void Start() {} // I don't know if I can delete an unused method like `Start()`.
+
+	// Update is called once per frame
+	void Update() {
+		horizontalInput = Input.GetAxis("Horizontal");
+		forwardInput = Input.GetAxis("Vertical"); // By "vertical", it means _forward_ - along the road.
+
+		transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+		transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+		//transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+	}
+}
